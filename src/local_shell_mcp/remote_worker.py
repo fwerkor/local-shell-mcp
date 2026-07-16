17ed8ceb77df65c15d3d288cbca834cccca1cdf6
Castronaut <3116935098@qq.com>
2026-07-16 13:43:25 +0800

    refactor: expose worker lifecycle subcommands in runtime

--- src/local_shell_mcp/remote_worker.py
+++ src/local_shell_mcp/remote_worker.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import sys
 
-from .remote import run_worker_cli
+from .worker_lifecycle import run_worker_cli
 
 if __name__ == "__main__":
     run_worker_cli(sys.argv[1:])
